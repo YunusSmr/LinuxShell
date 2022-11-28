46fdecf067e53abdc40fd0d443bf4abfff8ae8f6
YunusSmr <yunus.sumer@stu.fsm.edu.tr>
2022-11-29 00:25:13 +0300

    date added

--- writef.c
+++ writef.c
@@ -5,51 +5,58 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc,char ** argv)
+void *get_time(char *strDate)
 {
-    
-    if (argc!=3)
+
+    time_t sysytime = time(NULL);
+    char *time_str = ctime(&sysytime);
+    time_str[strlen(time_str) - 1] = '\0';
+    strcpy(strDate, time_str);
+    strDate[strlen(strDate)] = '\0';
+}
+int main(int argc, char **argv)
+{
+
+    if (argc != 3)
     {
-    printf("invalid number of paramter");
-    exit(0);
+        printf("invalid number of paramter");
+        exit(0);
     }
-   
-    if (strcmp(argv[1],"-f")==0)
+
+    if (strcmp(argv[1], "-f") == 0)
     {
-         
+
         FILE *fp;
+        char date[100];
+        // Open file in write mode
+        fp = fopen(argv[2], "a+");
 
-   // Open file in write mode
-   fp = fopen(argv[2],"a+");
-  
-   
-   // If file opened successfully, then write the string to file
-   if ( fp )
-   {
-        
-	   fprintf(fp,"pid değeri :%d\n",getpid());
-       fprintf(fp,"Ppid değeri :%d\n",getppid());
-        time_t t = time(NULL);
-        struct tm tm = *localtime(&t);
-        printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+        // If file opened successfully, then write the string to file
+        if (fp)
+        {
 
-       // fputs(tm,fp);
-       fputs("\n",fp);
-        
+            fprintf(fp, "Pid değeri: %d\n", getpid());
+            fprintf(fp, "Ppid değeri: %d\n", getppid());
+            get_time(date);
+            fprintf(fp, "System time: %s\n\n", date);
+            // time_t t = time(NULL);
+            // struct tm tm = *localtime(&t);
+            // printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+            // fputs(tm,fp);
+        }
+        else
+        {
+            printf("Failed to open the file\n");
+        }
+        // Close the file
+        fclose(fp);
     }
-   else
+    else
     {
-         printf("Failed to open the file\n");
-    }
-    //Close the file
-   fclose(fp);
-
-            
-    }else{
         printf("Wrong input");
         exit(0);
-
     }
-        
-return(0);
+
+    return (0);
 }
+
